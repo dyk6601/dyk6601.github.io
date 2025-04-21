63e4ec36c800f4651555fe1e47894d0bd131d75d
dyk6601 <dyk6601@gmail.com>
2025-04-21 18:39:12 -0400

    push

--- 001/Login/signup.js
+++ 001/Login/signup.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const supabaseClient = createClient(
-  'https://dyk6601.supabase.co',
+  'https://wrlxxfnvksegfehlvuls.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndybHh4Zm52a3NlZ2ZlaGx2dWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMDA1NTEsImV4cCI6MjA2MDc3NjU1MX0.kSmjtAe2N6NM591vTxX0W6namQGXIz7sZk3WSigg1wY'
 );
 
