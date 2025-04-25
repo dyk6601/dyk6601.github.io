7c1899946c8e70118cbc9df7477f448d44f18b65
dyk6601 <dyk6601@gmail.com>
2025-04-25 14:30:35 -0400

    login issue fix

--- 001/js/auth-check.js
+++ 001/js/auth-check.js
@@ -9,16 +9,16 @@ async function checkAuthStatus() {
         
         if (session) {
             // User is logged in
-            authLink.innerHTML = '<a href="../Login/logout.html">Logout</a>';
+            authLink.innerHTML = '<a href="./Login/logout.html">Logout</a>';
         } else {
             // User is not logged in
-            authLink.innerHTML = '<a href="../Login/login.html">Login</a>';
+            authLink.innerHTML = '<a href="./Login/login.html">Login</a>';
         }
     } catch (error) {
         console.error('Error checking auth status:', error);
         // Default to login link if there's an error
         const authLink = document.getElementById("auth-link");
-        authLink.innerHTML = '<a href="../001/login.html">Login</a>';
+        authLink.innerHTML = '<a href="./001/Login/login.html">Login</a>';
     }
 }
 
